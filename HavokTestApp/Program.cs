ae1b7ae2876cb679c3124eb3a61f3d048b52961b
Luke Kaalim <lukekaalim@gmail.com>
2023-05-19 02:09:32 +1000

    Remove tutorial name

--- HavokTestApp/Program.cs
+++ HavokTestApp/Program.cs
@@ -8,7 +8,7 @@ using OpenTK.Mathematics;
 var nativeWindowSettings = new NativeWindowSettings()
 {
     Size = new Vector2i(800, 600),
-    Title = "LearnOpenTK - Creating a Window",
+    Title = "OpenGL Thing",
     // This is needed to run on macos
     Flags = ContextFlags.ForwardCompatible,
 };
